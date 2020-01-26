48175d2f85e2437e685fe823aa0f3b4d7bfe7ca5
I-am-Harshdeep <beyourselfharsh@gmail.com>
2020-01-26 18:02:29 +0530

    adding menu

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Landing page of Restaurant using HTML CSS bootstrap and javascipt
 DEPENDENCIES:
 bootstrap,
 fontawesome,
-jqueyry3.4.1,
-jquery.ripples from https://github.com/sirxemic/jquery.ripples
+jquery3.4.1,
+jquery.ripples from https://github.com/sirxemic/jquery.ripples,
+magnific popup from https://github.com/dimsemenov/Magnific-Popup/
--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ $(document).ready(function(){
         dropRadius: 25,
         perturbance: 0.6,
       });
+    //magnific popup
+    $('.parent-container').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled: true
+        }
+        // other options
+      });
+
 });
