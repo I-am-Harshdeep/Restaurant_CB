e8599b740e547f67e7791d3a2e9d738e4fa50816
I-am-Harshdeep <beyourselfharsh@gmail.com>
2020-01-27 21:56:23 +0530

    final changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
 $(document).ready(function(){
+  //
+  $('.navbar-toggler').click(function(){
+    $('.navbar-toggler').toggleClass('change')
+  })
+  //sticky navbar less padding
+  $(window).scroll(function(){
+    
+    let postition = $(this).scrollTop();
+
+    if(postition>= 718){
+      $('.navbar').addClass('navbar-background');
+      $('.navbar').addClass('fixed-top');
+    }
+    else{
+      $('.navbar').removeClass('navbar-background');
+      $('.navbar').removeClass('fixed-top');
+    }
+    })
+
+    //smooth scroll
+    $('.nav-item a, .header-link, #back-to-top').click(function(link){
+      link.preventDefault();
+      
+      let target = $(this).attr('href');
+
+      $('html, body').stop().animate({
+        scrollTop: $(target).offset().top - 35
+      }, 3000);
+    })
+
+    //back-to-top
+    $(window).scroll(function(){
+    
+      let postition = $(this).scrollTop();
+  
+      if(postition>= 718){
+        $('#back-to-top').addClass('scrollTop');
+      }
+      else{
+        $('#back-to-top').removeClass('scrollTop');
+      }
+      })    
     //ripples
     $("header, info").ripples({
         dropRadius: 25,
